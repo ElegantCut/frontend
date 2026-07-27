99b63fb20de33693a45b29cbde98fa75e8d07fb6
EdwarStick <edwarsgl0318@gmail.com>
2026-07-27 16:10:24 -0500

    Bug(Respuesta de horario): Arreglo el formato de horario en que se visualizan todos los reultados en hora Bogota

--- Elegant-Cut-vite/src/routes/barber/BarberAppointments.jsx
+++ Elegant-Cut-vite/src/routes/barber/BarberAppointments.jsx
@@ -233,7 +233,8 @@ const BarberAppointments = () => {
                                                         weekday: 'long',
                                                         year: 'numeric',
                                                         month: 'long',
-                                                        day: 'numeric'
+                                                        day: 'numeric',
+                                                        timeZone: 'UTC'
                                                     })}
                                                 </span>
                                             </div>
